2edfd9a0cd4bc2e23363c78f2ba5d47349ccdff2
Sarthak S Kumar <sskworld9742@gmail.com>
2022-06-30 23:14:15 +0530

    Major Code Changes

--- Code/Algorithms/Ciphering/Caesar Cipher/Caesar_Cipher_enc.c
+++ Code/Algorithms/Ciphering/Caesar Cipher/Caesar_Cipher_enc.c
@@ -5,50 +5,40 @@ Last Updated: 05/06/2022
 Last Updated by: Shreya Mandi
 */
 
-//CAESAR CIPHER ENCODING
+// CAESAR CIPHER ENCODING
 #include <stdio.h>
 #include <string.h>
 char whitespace(char str[50]);
 
-int main()
-{
+int main() {
     printf("CAESER CIPHER ENCODING\n");
     printf("Enter the string and the shift.\n");
     int shift;
     char en_cc[50];
     char str[50];
-    gets(&str);
-    scanf("%d",&shift);
-    
-    
-    int i, len = 0,j;  
-    len = sizeof(str)/sizeof(str[0]);  
-    for(i = 0; i < len; i++){  
-        if(str[i] == ' '){  
-            for(j=i;j<len;j++)  
-        {  
-            str[j]=str[j+1];  
-        }  
-        len--;  
-        }  
-    }  
-    //printf("String after removing all the white spaces : %s", str);
-    
-    
-    for(int i=0; i<strlen(str); i++)
-    {
-        if(str[i]+shift>122)
-        {
-            int a=122-str[i];
-            int b=shift-a;
-            en_cc[i]=(char)(97+b);
-        }
-        else
-        {
-            en_cc[i]=(char)(str[i]+shift);
+    gets(str);
+    scanf("%d", &shift);
+
+    int i, len = 0, j;
+    len = sizeof(str) / sizeof(str[0]);
+    for (i = 0; i < len; i++) {
+        // if (str[i] == ' ') {
+        /*for (j = i; j < len; j++) {
+            str[j] = str[j + 1];
+            //}
+            len--;
+        }*/
+    }
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] + shift > 122) {
+            int a = 122 - str[i];
+            int b = shift - a;
+            en_cc[i] = (char)(97 + b);
+        } else {
+            en_cc[i] = (char)(str[i] + shift);
         }
     }
-    printf("The encoded string is:\n%s\n",en_cc);
+    printf("The encoded string is:\n%s\n", en_cc);
 
     return 0;
 }
